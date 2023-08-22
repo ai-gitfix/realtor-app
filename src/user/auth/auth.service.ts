9cce742f69a50843f6ea0c37ef335a1227645e21
Mutti <mustafa.atak@boun.edu.tr>
2023-08-22 22:53:06 +0300

    Creating a JWT token
    - Step 4 : Create a JWT token and return it for the user to use it in the site

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { UserType } from '@prisma/client';
 
 interface SignUpParams {
     email: string;
@@ -17,12 +20,34 @@ export class AuthService {
     async signUpUser({email, password, name, phone }: SignUpParams) {
 
         const IsUserExist = await this.prismaService.user.findUnique({ where: { email } });
-        console.log(IsUserExist);
 
         if (IsUserExist) {
             throw new ConflictException("User already exists");
         }
-        return ;
+
+        //Step3: Hash the password and create the user
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        const user = await this.prismaService.user.create({ 
+            data: {
+                name,
+                email,
+                phone,
+                password: hashedPassword,
+                user_type: UserType.BUYER,
+            }
+        });
+
+        
+        // npm install jsonwebtoken @types/jsonwebtoken
+        //Step 4 : Create a JWT token and return it
+        const token = jwt.sign(
+            { name, id: user.id }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '7d' });
+
+
+        return token;
     }
 
 }
